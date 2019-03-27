321400ff8d12b8eacb134b1fe07ef95cb50890e9
Unknown <mananlodhia@hotmail.com>
2019-03-27 20:54:22 +1100

    Updated jUnit
    Added more tests

--- GameOfThreads/src/JUnitTests/JUnitTest.java
+++ GameOfThreads/src/JUnitTests/JUnitTest.java
@@ -3,8 +3,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
-
-//Corrected imports
 import Model.*;
 import View.*;
 import Controller.*;
@@ -15,14 +13,46 @@ public class JUnitTest {
 	
 	Board board;
 	Square[][] grid;
+	Square sq;
+	int x,y;
+	Piece piece;
+	CornerSquare cS;
 	
 	@BeforeEach
 	public void setUp() throws Exception
 	{
-		board = new Board();
+		board = new Board(11,11);
 		grid = board.getBoard();
 		
+		sq = new Square(x,y);
+		cS = new CornerSquare(x, y);
+		
+		
 	}
+	
+	// Square Class Tests
+	
+	@Test
+	void occupiedTest() {
+		sq.setOccupied(true);
+		assertTrue(sq.getOccupied() == true);
+	}
+	
+	
+	@Test 
+	void checkPos() {
+		sq.setX(2);
+		sq.setY(1);
+		assertTrue(sq.getY() == 1 && sq.getX() == 2);
+	}
+	
+	
+	@Test
+	void pieceTest() {
+		sq.setPiece(piece);
+		assertNotNull(sq.getPiece() == piece);
+	}
+	
 
 	@Test // To check and see if the square is empty 
 	void checkOccupied()
@@ -37,16 +67,39 @@ public class JUnitTest {
 		}
 	}
 	
-	@Test // To check the position of the occupied squares
-	void checkOccupiedPos()
-	{
-		for (int i = 0; i < board.getHeight(); i++)
-		{
-			for (int j = 0; j < board.getWidth(); j++)
-			{
-				Square tempSq = board.getBoard()[j][i];
-				assertTrue(tempSq.getX() >= 0 && tempSq.getY() >= 0);
-			}
-		}
+	//Board Class Tests
+	
+	@Test
+	void widthAndHeightTest() {
+		
+		assertTrue(board.getWidth() == 11 && board.getHeight() == 11);
+	}
+	
+	@Test 
+	void squareTest() {
+		x = 2;
+		y = 3;
+		
+		board.setSquare(x, y, sq);
+		assertTrue(board.getSquare(x, y) != null );
+		
+	}
+	
+	@Test
+	void squarePieceTest() {
+		x = 2;
+		y = 3;
+		
+		board.setSquarePiece(x, y, piece);
+		assertTrue(board.getSquarePiece(x, y) != null);
+	}
+	
+	//CornerSquare Test
+	
+	@Test
+	void capturedTest() {
+		cS.setCaptured(true, 1);
+		assertTrue(cS.getCaptureStatus() == true && cS.getCapturedTeam() == 1);
+
 	}
 }
