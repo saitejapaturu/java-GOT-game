4c7e9cfc14b2a86cab0be4fdd0730da168d66c66
Unknown <saitejapaturu@gmail.com>
2019-05-26 17:22:04 +1000

    Added undo, redo to history

--- GameOfThreads/src/Controller/BoardHistory.java
+++ GameOfThreads/src/Controller/BoardHistory.java
@@ -4,26 +4,87 @@ import java.util.List;
 
 import Model.Board;
 
-public class BoardHistory{
+public class BoardHistory
+{
 
-	List <Board> history;
-	public BoardHistory()
+	private List <Board> history;
+	private int currentTurn;		// Stores Which move we are currently at.
+	private int turnCounter;
+
+	public BoardHistory(Board board)
 	{
+		this.currentTurn = 0;
+		this.history.add(currentTurn, board);
+		turnCounter = 0;
 		
 	}
-	
-	public Board getBoardAtTurn(int turn)
+
+	// If undo is possible, returns the last turn.
+	public Board undo()
 	{
-		return history.get(turn);
+		if(currentTurn==0)
+		{
+			return null;
+		}
+		else
+		{
+			currentTurn--;
+			return this.history.get(currentTurn);
+		}
 	}
-	
-	public void push(Board board)
+
+	// If a redo is possible, returns the turn made before calling undo.
+	public Board redo()
 	{
-		history.add(board);
+		if(currentTurn==turnCounter)
+		{
+			return null;
+		}
+		else
+		{
+			currentTurn++;
+			return this.history.get(currentTurn);
+		}
 	}
-	
-	public void pop()
+
+	// Adds the current board state to history
+	public void moveMade(Board board)
 	{
-		history.remove(history.size()-1);
+		// move Made after undo
+		if(currentTurn<turnCounter)
+		{
+			moveMadeAfterUndo();
+		}
+
+		//Add current move
+		currentTurn++;
+		turnCounter++;
+		this.history.add(currentTurn, board);
+
 	}
+
+	// If a move is made after undo, then the future moves that were made before using undo are removed.
+	private void moveMadeAfterUndo()
+	{
+		while(turnCounter > currentTurn)
+		{
+			history.remove(turnCounter);
+			turnCounter--;
+		}
+	}
+
+//	public Board getBoardAtTurn(int turn)
+//	{
+//		return history.get(turn);
+//	}
+	
+//	public void push(Board board)
+//	{
+//		history.add(board);
+//	}
+	
+//	public void pop()
+//	{
+//		history.remove(history.size()-1);
+//	}
 }
